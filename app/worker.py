09567bff41919f8d2ac96ec4a5339e52ce71ec22
Yorukot <hi@yorukot.me>
2026-03-11 01:20:20 +0800

    fix: use int8_float32 for GTX 1080 (CC 6.1) which does not support float16

--- app/worker.py
+++ app/worker.py
@@ -57,7 +57,7 @@ class WhisperRunner:
             model_name,
             device="cuda",
             device_index=0,
-            compute_type="int8",
+            compute_type="int8_float32",
         )
         self.cc = OpenCC("s2t")
 
